1ce3592069337374ff1e9b570f03486b525a453f
Steve Choi <stevechoi93@gmail.com>
2018-12-20 16:59:59 -0800

    initial commit

---
+++ adp-entrance/src/Components/Buttons.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+class Buttons extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Default</Button>
+        <Button>Default</Button>
+      </div>
+    );
+  }
+}
+
+export default Buttons;
